d63f19ed559092709a957b1995db4f49f3de2215
IMPROBABLYBETTERTHANYOUJK <57474725+IMPROBABLYBETTERTHANYOUJK@users.noreply.github.com>
2020-04-01 05:37:25 -0700

    Update TB.py

--- TB.py
+++ TB.py
@@ -1,7 +1,7 @@
 import discord
 import os
 
-port = int(os.environ.get('PORT', 5000))
+#port = int(os.environ.get('PORT', 5000))
 
 client = discord.Client()
 
@@ -19,8 +19,5 @@ class MyClient(discord.Client):
 
         if message.content.startswith('$$hello'):
             await message.channel.send('Hello {0.author.mention}'.format(message))
-        elif message.content.startswith('$$about'):
-            await message.channel.send('This bot was made by Conner Black for the purposes of Stock Trading. Any questions about it must be dmed to {0.ConnerBlack.mention}'.format(message))
-            
 
 client.run('NjQwMTA0OTg3MDY1NTgxNTgz.XoSBbg.VW3b3Zh_0nKsrOu-4FZtRgGSGvE')
